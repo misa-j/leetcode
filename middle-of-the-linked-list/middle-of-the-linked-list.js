f190d68dcf152b1972bce6a058ec4513bd06a30c
Misa Jakovljevic <misajakovljevic3@gmail.com>
2021-12-29 14:40:59 +0100

    Time: 76 ms (45.42%), Space: 38.8 MB (63.81%) - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let p1 = head;
+    let p2 = head;
+    
+    while(p2 && p2.next) {
+        p1 = p1.next;
+        p2 = p2.next.next;
+    }
+    
+    return p1;
+};
