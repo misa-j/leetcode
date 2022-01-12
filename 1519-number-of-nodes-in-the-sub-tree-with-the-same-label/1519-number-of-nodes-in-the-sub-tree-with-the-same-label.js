f301b819017574686e36770d0c26f3691eb38a1c
Misa Jakovljevic <misajakovljevic3@gmail.com>
2022-01-12 14:50:19 +0100

    Time: 668 ms (80.00%), Space: 121.5 MB (80.00%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.js
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.js
@@ -5,21 +5,21 @@
  * @return {number[]}
  */
 var countSubTrees = function(n, edges, labels) {
-    let graph = new Array(n).fill([]);
-    let seen = new Array(n).fill(false);
-    let res = new Array(n).fill(1);
+    let graph = new Array(n);
+    let res = new Array(n).fill(0);
     
     for(let i = 0; i < edges.length; i++) {
         const [x, y] = edges[i];
-        if(graph[x].length === 0) graph[x] = [];
-        if(graph[y].length === 0) graph[y] = [];
+        if(!graph[x]) graph[x] = [];
+        if(!graph[y]) graph[y] = [];
         graph[x].push(y);
         graph[y].push(x);
     }
     
     function traverse(node) {
-        if(seen[node]) return null;
-        seen[node] = true;
+        if(res[node]) return null;
+        res[node] = 1;
+        
         const nodes = graph[node];
         let count = new Array(26).fill(0);
         const char = labels[node].charCodeAt() - 97;
@@ -32,7 +32,7 @@ var countSubTrees = function(n, edges, labels) {
             }
         }
         
-        res[node] = count[char] || 1;
+        res[node] += count[char] - 1;
         return count;
     }
     traverse(0);
