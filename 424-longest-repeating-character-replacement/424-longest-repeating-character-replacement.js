c43502176c5705cbce6df4e0ce0d5473305090e6
Misa Jakovljevic <misajakovljevic3@gmail.com>
2022-01-31 02:44:18 +0100

    Time: 132 ms (49.17%), Space: 42.6 MB (27.50%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -4,41 +4,24 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
+    const map = new Array(26).fill(0);
+    let left = 0;
     let res = 0;
-    const map = new Array(26);
-    for(let i = 0; i < 26; i++) map[i] = [];
-    
-    let prev = 0;
-    for(let i = 0; i < s.length - 1; i++) {
-        if(s[i] !== s[i + 1]) {
-            const index = s[i].charCodeAt() - 65;
-            map[index].push(prev);
-            prev = i + 1;
-        }
-    }
-    const index = s[s.length - 1].charCodeAt() - 65;
-    map[index].push(prev);
-    
-    function maxLen(list, char) {
-        let res = 0;
-        for(let j = 0; j < list.length; j++) {
-            let sum = 0;
-            let n = k;
-            for(let i = list[j]; i < s.length; i++) {
-                if(s[i] !== char) n--;
-                if(n < 0) break;
-                sum++;
+    let freq = 0;
+    for(let i = 0; i < s.length; i++) {
+        map[s[i].charCodeAt() - 65]++;
+        freq = Math.max(freq, map[s[i].charCodeAt() - 65]);
+        const length = (i - left) + 1;
+        //const max = Math.max(...map);
+        if(length - freq <= k) res = Math.max(res, length);
+        else {
+            while(left <= i) {
+                map[s[left++].charCodeAt() - 65]--;
+                const length = (i - left) + 1;
+                const max = Math.max(...map);
+                if(length - max <= k) break;
             }
-            if(n > 0) sum += n;
-            res = Math.max(res, sum);
         }
-        
-        return Math.min(res, s.length);
-    }
-    
-    for(let i = 0; i < 26; i++) {
-        if(map[i].length) res = Math.max(res, maxLen(map[i], s[map[i][0]]));
-        if(res === s.length) break;
     }
     
     return res;
